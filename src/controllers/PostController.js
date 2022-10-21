d1fefe47e2b33036959f2f80290945ddfed30698
kikilove5me <77271656+kikilove5me@users.noreply.github.com>
2022-10-22 01:03:19 +0200

    modified Post

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,7 +2,7 @@ import PostService from "../service/PostService.js";
 class PostController {
     async create(req, res) {
         try {
-            //console.log(req.files);
+            console.log(req.files);
             const post = await PostService.create(req.body, req.files.picture);
             res.status(200).json(post)
         } catch (e) {
